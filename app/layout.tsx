1dd48a831aee747a7dbb4bf45bfecce753833af7
DombPolar <shindh0525@gmail.com>
2024-06-03 20:23:41 +0900

    prisma

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,5 @@
 import type { Metadata } from "next";
-import { Roboto, Rubik_Scribble } from "next/font/google";
 import "./globals.css";
-
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "500"],
-  style: ["normal", "italic"],
-  variable: "--roboto-text",
-});
-
-const rubik = Rubik_Scribble({
-  subsets: ["latin"],
-  weight: ["400"],
-  style: ["normal"],
-  variable: "--rubik-text",
-});
 
 export const metadata: Metadata = {
   title: {
@@ -31,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${roboto.variable} ${rubik.variable} bg-neutral-900 text-white max-w-md mx-auto`}
-      >
+      <body className="bg-neutral-900 text-white max-w-md mx-auto">
         {children}
       </body>
     </html>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+const { withFonts } = require('next-fonts');
+
+module.exports = withFonts({
+  webpack(config) {
+    return config;
+  },
+});
+
 const nextConfig = {
   experimental: {
     taint: true,
